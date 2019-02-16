992580aeceb5d9d40069d01d25b275d030553af9
Antony.Glim <Antony.Glim@gmail.com>
2019-02-16 10:51:32 +0300

    Singleton add

---
+++ src/main/java/singleton/SingletonExample.java
@@ -0,0 +1,14 @@
+package singleton;
+
+public class SingletonExample {
+
+    private static SingletonExample instance;
+
+    private SingletonExample() {
+    }
+
+    public static synchronized SingletonExample getInstance(){
+        if (instance == null) instance = new SingletonExample();
+        return instance;
+    }
+}
